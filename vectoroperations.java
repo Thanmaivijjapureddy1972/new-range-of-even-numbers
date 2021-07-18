1349bbe73bb726411167de5bab2d159c6c946aa6
Thanmaivijjapureddy1972 <86625282+Thanmaivijjapureddy1972@users.noreply.github.com>
2021-07-17 23:05:13 -0700

    Create vectoroperations.java

---
+++ vectoroperations.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class v1
+{
+ public static void main(String args[])
+ {
+  Vector v=new Vector();
+  v.add(101);
+  v.add("Rama");
+  v.add(6.58);
+  v.add(102);
+  v.add("Gita");
+  v.add(8.98);
+  System.out.println(v);
+  v.remove(4);
+  System.out.println(v);
+  v.set(2,9.17);
+  System.out.println(v);
+  v.contains("Sita");
+ System.out.println(v.contains("Sita"));
+}
+}
